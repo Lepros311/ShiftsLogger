1be784dffadcab7fb7d6b1132eb7b0abb4657781
Lepros311 <andy.strickland@gmail.com>
2025-06-05 11:07:03 -0400

    Add Views folder with Display and UserInterface classes

---
+++ Views/Display.cs
@@ -0,0 +1,50 @@
+﻿using ShiftsLogger.Models;
+using Spectre.Console;
+
+namespace ShiftsLogger.Views;
+
+public class Display
+{
+    public static void PrintShiftsTable(List<Shift> shifts, string heading)
+    {
+        Console.Clear();
+
+        var rule = new Rule($"[green]{heading}[/]");
+        rule.Justification = Justify.Left;
+        AnsiConsole.Write(rule);
+
+        var table = new Table()
+            .Border(TableBorder.Rounded)
+            .AddColumn(new TableColumn("[dodgerblue1]Shift Name[/]"))
+            .AddColumn(new TableColumn("[dodgerblue1]Phone Number[/]"))
+            .AddColumn(new TableColumn("[dodgerblue1]Email Address[/]"))
+            .AddColumn(new TableColumn("[dodgerblue1]Duration[/]"))
+            .AddColumn(new TableColumn("[dodgerblue1]Worker[/]"));
+
+        foreach (Shift shift in shifts)
+        {
+            table.AddRow(shift.ShiftName, shift.StartTime.ToString(), shift.EndTime.ToString(), shift.Duration.ToString(), shift.Worker.FirstName);
+        }
+
+        AnsiConsole.Write(table);
+    }
+
+    static internal void PrintWorkersTable(List<Worker> workers, string heading)
+    {
+        Console.Clear();
+
+        var rule = new Rule($"[green]{heading}[/]");
+        rule.Justification = Justify.Left;
+        AnsiConsole.Write(rule);
+
+        var table = new Table();
+        table.AddColumn("[dodgerblue1]Worker Name[/]");
+
+        foreach (Worker worker in workers)
+        {
+            table.AddRow(worker.ToString());
+        }
+
+        AnsiConsole.Write(table);
+    }
+}

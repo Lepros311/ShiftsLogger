42e56d3a73dca9a0074bf0002566043286b3f03f
Lepros311 <andy.strickland@gmail.com>
2025-06-04 20:51:22 -0400

    Successful GET requests

--- Data/ShiftsDbContext.cs
+++ Data/ShiftsDbContext.cs
@@ -5,6 +5,7 @@ namespace ShiftsLogger.Data;
 
 public class ShiftsDbContext : DbContext
 {
+    public ShiftsDbContext(DbContextOptions options) : base(options) { }
     public DbSet<Shift> Shifts { get; set; }
 
     public DbSet<Worker> Workers { get; set; }
@@ -56,64 +57,72 @@ public class ShiftsDbContext : DbContext
                     ShiftId = 1,
                     WorkerId = 1,
                     ShiftName = "1st",
-                    StartTime = DateTime.Now,
-                    EndTime = DateTime.Now
+                    StartTime = new DateTime(2023, 10, 15, 08, 00, 0),
+                    EndTime = new DateTime(2023, 10, 15, 16, 00, 0),
+                    Duration = 8
                 },
                 new Shift
                 {
                     ShiftId = 2,
                     WorkerId = 1,
                     ShiftName = "2nd",
-                    StartTime = DateTime.Now,
-                    EndTime = DateTime.Now
+                    StartTime = new DateTime(2023, 10, 15, 16, 00, 0),
+                    EndTime = new DateTime(2023, 10, 15, 00, 00, 0),
+                    Duration = 8
                 },
                 new Shift
                 {
                     ShiftId = 3,
                     WorkerId = 2,
                     ShiftName = "2nd",
-                    StartTime = DateTime.Now,
-                    EndTime = DateTime.Now
+                    StartTime = new DateTime(2023, 10, 15, 16, 00, 0),
+                    EndTime = new DateTime(2023, 10, 15, 00, 00, 0),
+                    Duration = 8
                 },
                 new Shift
                 {
                     ShiftId = 4,
                     WorkerId = 2,
                     ShiftName = "3rd",
-                    StartTime = DateTime.Now,
-                    EndTime = DateTime.Now
+                    StartTime = new DateTime(2023, 10, 15, 00, 00, 0),
+                    EndTime = new DateTime(2023, 10, 15, 08, 00, 0),
+                    Duration = 8
                 },
                 new Shift
                 {
                     ShiftId = 5,
                     WorkerId = 3,
                     ShiftName = "3rd",
-                    StartTime = DateTime.Now,
-                    EndTime = DateTime.Now
+                    StartTime = new DateTime(2023, 10, 15, 00, 00, 0),
+                    EndTime = new DateTime(2023, 10, 15, 08, 00, 0),
+                    Duration = 8
                 },
                 new Shift
                 {
                     ShiftId = 6,
                     WorkerId = 3,
                     ShiftName = "2nd",
-                    StartTime = DateTime.Now,
-                    EndTime = DateTime.Now
+                    StartTime = new DateTime(2023, 10, 15, 16, 00, 0),
+                    EndTime = new DateTime(2023, 10, 15, 00, 00, 0),
+                    Duration = 8
                 },
                 new Shift
                 {
                     ShiftId = 7,
                     WorkerId = 4,
                     ShiftName = "1st",
-                    StartTime = DateTime.Now,
-                    EndTime = DateTime.Now
+                    StartTime = new DateTime(2023, 10, 15, 08, 00, 0),
+                    EndTime = new DateTime(2023, 10, 15, 16, 00, 0),
+                    Duration = 8
                 },
                 new Shift
                 {
                     ShiftId = 8,
                     WorkerId = 4,
                     ShiftName = "1st",
-                    StartTime = DateTime.Now,
-                    EndTime = DateTime.Now
+                    StartTime = new DateTime(2023, 10, 15, 08, 00, 0),
+                    EndTime = new DateTime(2023, 10, 15, 16, 00, 0),
+                    Duration = 8
                 }
            });
     }
